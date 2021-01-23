f1b64e2e1fba010288f915d0969f8f55173c3c58
Jacob Della <Jake.della3@gmail.com>
2021-01-23 14:25:19 -0500

    Removed uneccesary database stuff

--- jdella1996-frontend/src/App.js
+++ jdella1996-frontend/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import Blog from './components/Blog';
-import Contact from './components/Contact';
 import Navigation from './components/Navigation';
 import Links from './components/Links';
 import Gallery from './components/Gallery';
@@ -35,9 +33,7 @@ class App extends Component {
               >
                 <Switch location = {location}>
                     <Route path="/" component={Home} exact/>
-                    <Route path="/contact" component={Contact} exact/>
                     <Route path="/links" component={Links} exact/>
-                    <Route path="/blog" component={Blog} exact/>
                     <Route path="/gallery" component={Gallery} exact/>
                     <Route component={Error}/>
                   </Switch>
--- jdella1996-frontend/src/components/Home.js
+++ jdella1996-frontend/src/components/Home.js
@@ -1,52 +1,24 @@
 import React,{Component} from 'react';
 import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
-
+import quotes from './staticfiles/json/quotes.json';
 class Home extends Component{
     
     constructor(props)
     {
         super(props);
-        this.state = {
-            quotes : []
-        };
     }
 
-    
-    async componentDidMount() {
-        // Call our fetch function below once the component mounts
-      await this.callBackendAPI()
-        .then(res => this.setState({ quotes: res }))
-        .catch(err => console.log(err));
-      }
-
-      callBackendAPI = async () => {
-        const response = await fetch('/api/displayquotes' , {
-          method: "POST",
-          headers: {
-              'Content-type': 'application/json'
-          },
-          body: JSON.stringify(this.state)
-          });
-        const body = await response.json();
-  
-        if (response.status !== 200) {
-          throw Error(body.message) 
-        }
-        return body;
-      };
-
       getQuotes() {
-        let RandomQuote = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)];
-        let Triggered = false;
-        return this.state.quotes.map((quote) => {
-        if (quote  == RandomQuote & Triggered == false){
-            Triggered = true;
+        let RandomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        let triggered = false
+        return quotes.map((quote) => {
+        if (!triggered){
+            triggered = true
             return (
-
                 <div>
                 <h4 className='frontpage'>
                     <h3>Random quote of the day</h3>
-                  {quote.quote_author} - "{quote.quote_contents}" 
+                  {RandomQuote.quote_author} - "{RandomQuote.quote_contents}" 
                 
                         
                  </h4>
@@ -74,9 +46,9 @@ class Home extends Component{
         <hr class = "greenbar"></hr>
         <h1>{this.getQuotes()}</h1>
         <hr class = "greenbar"></hr>
-        <p>My name is Jacob Della, welcome to my site! I am currently a student enrolled at Towson University, enrolled in their Computer Science program. and minoring in Math. My graduation date should be by sometime in 2021. 
+        <p>Hello! My name is Jacob Della, welcome to my site! I graduated from Towson University with a bachelors of Computer Science and a Mathematics minor.
           I have numerous years of IT and Software experience doing projects at home and work. I generally tend to focus my efforts on more open technology stacks such as; Python, JavaScript, C++, and Java.If you were 
-          around for the original version of this website it used Django, it has now been rewritten to utilize React, Express API and a MySQL docker image. This website is hosted at my house in a Ubuntu VM using Docker images.
+          around for the original version of this website it used Django, it has now been rewritten to utilize React. This new version of the website dumped the use of an external database system since it was pretty overkill.
           All the important stuff is under links.</p>
           <hr class = "greenbar"></hr>
           <h1>Tweets by yours truly.</h1>
@@ -89,8 +61,6 @@ class Home extends Component{
           />
           </a>
         <p>By JDella1996</p>
-        <p>Hosted on GRAVEYARD going 2 years strong.</p>
-        <p>Last Updated 02/2020</p>
         </section>
        </div>
         );
